f95c0c29ea15154a8be97a963b3e0cd72d8c23b2
Matty <mattydev12@gmail.com>
2012-07-10 17:55:26 -0700

    Round 2

--- source/js/classes/unit.js
+++ source/js/classes/unit.js
@@ -1,25 +1,46 @@
 function Unit (color) {
 	this.color = color;
+	this.size = getRandomInt(1, 5);
+	this.lifespan = getRandomInt(1, 20000);
     this.x=getRandomInt(0, 500);
 	this.y=getRandomInt(0, 400);
-	this.dx=5;
-	this.dy=5;
+
+	function UnitOutput()
+	{
+		this.duplicate = (getRandomInt(0, 2000) == 1);
+	}
+
+	this.process = function (context)
+	{
+		var output = new UnitOutput();
+		this.draw(context);
+		return output;
+	}
 
 	this.draw = function (context)
 	{
 		context.beginPath();
 		context.fillStyle=this.color;
 		// Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-		context.arc(this.x,this.y,10,0,Math.PI*2,true);
+		context.arc(this.x,this.y,this.size,0,Math.PI*2,true);
 		context.closePath();
 		context.fill();
 		
 		// Boundary Logic
-		if( this.x<0 || this.x>500) this.dx=-this.dx;
-		if( this.y<0 || this.y>400) this.dy=-this.dy;
-		
-		this.x+=this.dx;
-		this.y+=this.dy;
+		if (getRandomInt(0, 200) == 1)
+		{
+			var dx=getRandomInt(-5, 5);
+			var dy=getRandomInt(-5, 5);
+
+			if(!((this.x<=0 && dx < 0)  
+				|| (this.x>=500 && dx > 0) 
+				|| (this.y<=0 && dy < 0) 
+				|| (this.y>=400 && dy > 0)))
+			{
+				this.x+=dx;
+				this.y+=dy;
+			}
+		}
 	}
 }
 
